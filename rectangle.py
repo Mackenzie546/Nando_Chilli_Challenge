6d4aa6113415acf47781627b99b60a439c9090f9
Mackenzie <mackenzie@users-iMac.local>
2018-09-12 21:14:28 +0100

    Rectangle better

--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,19 @@
+def get_input (prompt, accepted):
+    while True:
+        value = input (prompt).lower()
+
+        if value in accepted:
+            return value
+        else:
+            print ("that is not a recognised answer, must be one of ", accepted)
+
 print ("i can do geometry on a rectangle")
-length = input ("what is the length of your rectangle?")
-width = input  ("what is the width of your rectangle?")
-area = length * width
-print "area:", area
-perimeter = (length * 2) + (width * 2)
-print "perimeter:", perimeter
+length = int(input ("what is the length of your rectangle?"))
+width = int(input  ("what is the width of your rectangle?"))
+mode = get_input ("do you want to know the area or perimeter?", ["area","perimeter"])
+if mode == "area":
+    area = length * width
+    print ("area:", area)
+elif mode == "perimeter":
+    perimeter = (length * 2) + (width * 2)
+    print ("perimeter:", perimeter)
