9dbf2f66f027bb5bc745a98e9bcd47bd087ccb17
Mackenzie <mackenzie@users-iMac.local>
2018-09-12 20:51:24 +0100

    rectangle basic

---
+++ rectangle.py
@@ -0,0 +1,7 @@
+print ("i can do geometry on a rectangle")
+length = input ("what is the length of your rectangle?")
+width = input  ("what is the width of your rectangle?")
+area = length * width
+print "area:", area
+perimeter = (length * 2) + (width * 2)
+print "perimeter:", perimeter
